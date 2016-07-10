9f8bd703fb817ea2ae09234ebec2537fc449cf3b
teirm <tribunus.vii@gmail.com>
2016-07-10 14:34:29 -0400

    Pretty printed summary

--- web_scrapers/quick_wiki.py
+++ web_scrapers/quick_wiki.py
@@ -5,6 +5,8 @@ Purpose: Given user input, cleanly display the wikipedia summary for the input.
 Author: Cyrus Ramavarapu
 Date: 10 July 2016
 """
+import pprint
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -20,13 +22,15 @@ def get_content(section_url):
 
     Returns: None
     """
-
+    summary_pp = pprint.PrettyPrinter(width=80)
     proxy = {'http':section_url}
     response = requests.get(section_url, proxies=proxy)
     soup = BeautifulSoup(response.text, 'lxml')
     body_text = soup.find('div', {'id': 'mw-content-text'})
     paragraphs = body_text.find_all('p')
-    print(paragraphs[0].text)
+    summary = paragraphs[0].text
+    summary_pp.pprint(summary)
+    print(section_url)
 
 if __name__ == '__main__':
     get_content(BASE_URL)
